754dfd9aa86f0bb8d2ad4395497a54d220e8fc75
harismh <harismh@icloud.com>
2016-11-02 16:35:04 -0700

    Passed specs for functional-shared type for stack and queue; pair with Dunstan and Haris

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,59 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  instance.length = 0;
+
+  instance.size = queueMethods.size;
+  instance.enqueue = queueMethods.enqueue;
+  instance.dequeue = queueMethods.dequeue;
+  instance.return = queueMethods.return;
+
+  return instance;
+
+
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+  return this.length;
+};
 
+queueMethods.enqueue = function(value) {
+  var storage = this.storage;
+  var length = this.length;
+
+  if (length === 0) {
+    storage[0] = value;
+    this.length++;
+    return value;
+  } else {
+    var keys = []; 
+    var values = [];
+    for (var key in storage) {
+      keys.push(key);
+      values.push(storage[key]);
+    }
+    for (var i = 0; i < keys.length; i++) {
+      storage[parseInt(keys[i]) + 1] = values[i];
+    }
+    storage[0] = value;
+    this.length++;
+    return value;
+  }
+};
+
+queueMethods.dequeue = function() {
+  if (this.length !== 0) {
+    var value = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length--;
+    return value;
+  }
+};
+
+queueMethods.return = function() {
+  return this.storage;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,50 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  instance.length = 0;
+
+  instance.size = stackMethods.size;
+  instance.push = stackMethods.push;
+  instance.return = stackMethods.return;
+  instance.pop = stackMethods.pop;
+
+
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return this.length;
+};
 
+stackMethods.push = function(value) {
+  var storage = this.storage;
+  var length = this.length;
+
+  if (length === 0) {
+    storage[length] = value;
+    this.length++; 
+    return;
+  } else {
+    storage[length] = value;
+  }
+
+  this.length++;
+  
+};
+
+stackMethods.pop = function() {
+  if (this.length !== 0) {
+    var value = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length--;
+    return value;
+  }
+};
+
+stackMethods.return = function() {
+  return this.storage;
+};
