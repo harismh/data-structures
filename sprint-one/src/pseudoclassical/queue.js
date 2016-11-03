c26daee1817df81e9114426bf2beb414fbb7d439
harismh <harismh@icloud.com>
2016-11-02 18:51:09 -0700

    Passed specs for psuedoclassical; pair Dunstan and Haris

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,52 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+
+  this.storage = {};
+  this.length = 0;
+
 };
 
+var queueMethods = {};
 
+Queue.prototype.size = function() {
+  return this.length;
+};
+
+Queue.prototype.enqueue = function(value) {
+  var storage = this.storage;
+  var length = this.length;
+
+  if (length === 0) {
+    storage[0] = value;
+    this.length++;
+    return value;
+  } else {
+    var keys = []; 
+    var values = [];
+    for (var key in storage) {
+      keys.push(key);
+      values.push(storage[key]);
+    }
+    for (var i = 0; i < keys.length; i++) {
+      storage[parseInt(keys[i]) + 1] = values[i];
+    }
+    storage[0] = value;
+    this.length++;
+    return value;
+  }
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.length !== 0) {
+    var value = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length--;
+    return value;
+  }
+};
+
+Queue.prototype.return = function() {
+  return this.storage;
+};
