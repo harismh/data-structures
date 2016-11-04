fba9ef6a2e3f6996b2cb66bf32aba36e203509d9
harismh <harismh@icloud.com>
2016-11-04 11:33:11 -0700

    Passed specs for set; pair Dunstan and Haris

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,20 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return !!this[item];
 };
 
 setPrototype.remove = function(item) {
+  delete this[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  add: constant O(1);
+  contains: constant O(1);
+  remove: constant O(1);
  */
