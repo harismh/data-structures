a163385d545d93a14e01e0ea42d78fa29b4953ae
harismh <harismh@icloud.com>
2016-11-04 18:49:03 -0700

    Passed all specs for binarySearchTree; pair Dunstan and Haris

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,9 @@
 var BinarySearchTree = function(value) {
   var instance = Object.create(BinarySearchTreeMethods);
-  instance.tree = {};
-  instance.tree.start = {value: value, left: null, right: null};
+  
+  instance.value = value;
+  instance.left = null;
+  instance.right = null;
   
   return instance;
 };
@@ -11,33 +13,66 @@ var BinarySearchTreeMethods = {};
 BinarySearchTreeMethods.insert = function(value) {
   var node = {value: value, left: null, right: null};
   
-  var recurse = function(pointer) {
-    if (value > pointer.value) {
-      if (pointer.right === null) {
-        pointer.right = node;
+  var recurse = function(current) {
+    if (value > current.value) {
+      if (current.right === null) {
+        current.right = node;
       } else {
-        pointer = pointer.right;
-        recurse(pointer);      
+        current = current.right;
+        recurse(current);      
       }
     } else {
-      if (pointer.left === null) {
-        pointer.left = node;
+      if (current.left === null) {
+        current.left = node;
       } else {
-        pointer = pointer.left;
-        recurse(pointer);
+        current = current.left;
+        recurse(current);
       }
     }
   };
 
-  return recurse(this.tree.start);
+  return recurse(this);
 };
 
 BinarySearchTreeMethods.contains = function(target) {
+  var result = false;
 
+  var recurse = function(current) {
+    if (current.value === target) {
+      result = true;
+    } else if (current.value < target && current.right !== null) {
+      current = current.right;
+      recurse(current);
+    } else if (current.left !== null) {
+      current = current.left;
+      recruse(current);
+    }
+  };
+  
+  recurse(this);
+  return result;
 };
 
 BinarySearchTreeMethods.depthFirstLog = function(cb) {
+  var calledOnce = false;
+  
+  var recurse = function(current) {
+    if (!calledOnce) {
+      calledOnce = true;
+      cb(current.value);
+    } 
+    if (current.left !== null) {
+      current = current.left;
+      calledOnce = false;
+      recurse(current);
+    } else if (current.right !== null) {
+      current = current.right;
+      calledOnce = false;
+      recurse(current);
+    }
+  };
 
+  recurse(this);
 };
 
 /*
